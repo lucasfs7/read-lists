105caad16a0f80fb583d42dda19ba668b62f61c4
lucasfs7 <lucasfs7@gmail.com>
2017-02-21 15:41:16 -0300

    add tests to lists reducer

--- webapp/src/reducers/Lists.test.js
+++ webapp/src/reducers/Lists.test.js
@@ -1,10 +1,63 @@
-import { reducer, request } from 'reducers/Lists'
+import Reducer, * as actions from 'reducers/Lists'
 
-it('should return type REQUEST', () => {
-  expect(request()).toEqual({ type: 'REQUEST' })
+test('create a new list', () => {
+  const list = { name: 'new list', links: [] }
+  const state = Reducer(
+    undefined,
+    { type: actions.create, payload: list },
+  )
+  expect(state).toEqual({ lists: [ list ] })
 })
 
-it('should change loading status when request', () => {
-  const state = reducer({}, request())
-  expect(state).toEqual({ loading: true })
+test('update a list', () => {
+  const list = { name: 'list 1', links: [], id: 1 }
+  const updatedList = { ...list, links: [ 'link 1' ] }
+  const state = Reducer(
+    { lists: [ list ] },
+    { type: actions.update, payload: updatedList },
+  )
+  expect(state).toEqual({ lists: [ updatedList ] })
+})
+
+test('remove a list', () => {
+  const list = { name: 'new list', links: [], id: 1 }
+  const state = Reducer(
+    { lists: [ list ] },
+    { type: actions.remove, payload: list },
+  )
+  expect(state).toEqual({ lists: [] })
+})
+
+test('load a list inside empty list', () => {
+  const list = { name: 'list 3', links: [], id: 3 }
+  const state = Reducer(
+    undefined,
+    { type: actions.load, payload: list },
+  )
+  expect(state).toEqual({ lists: [ list ] })
+})
+
+test('load a list inside an existing array of lists', () => {
+  const lists = [
+    { name: 'list 1', links: [], id: 1 },
+    { name: 'list 2', links: [], id: 2 }
+  ]
+  const newList = { name: 'list 3', links: [], id: 3 }
+  const state = Reducer(
+    { lists },
+    { type: actions.load, payload: newList },
+  )
+  expect(state).toEqual({ lists: [ ...lists, newList ] })
+})
+
+test('load all lists', () => {
+  const lists = [
+    { name: 'list 1', links: [], id: 1 },
+    { name: 'list 2', links: [], id: 2 },
+  ]
+  const state = Reducer(
+    undefined,
+    { type: actions.loadAll, payload: lists },
+  )
+  expect(state).toEqual({ lists })
 })
